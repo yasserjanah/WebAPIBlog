a76a9f3acfd6855d2384c24c08ccc55cad4b1248
Yasser JANAH <contact@yasser-janah.com>
2021-05-31 18:12:47 +0100

    add functions to * routes/users.js *

--- JANAH/routes/users.js
+++ JANAH/routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const usersRepo = require('../repositories/users');
+
+router.get('/', async function(req, res, next) {
+	let response = await (req.query.offset && req.query.limit) ? usersRepo.getUsers(req.query.offset, req.query.limit) : usersRepo.getAllUsers()
+  	res.send(await response);
+});
+
+router.get('/:id', async function(req, res, next) {
+  	res.send(await usersRepo.getUser(req.params.id));
+});
+
+router.post('/', async function(req, res, next) {
+  	let user = {};
+  	user.username = req.body.username;
+  	user.email = req.body.email;
+  	user.password = req.body.password;
+  	user.role = req.body.role;
+  	res.send(await usersRepo.addUser(user));
+});
+
+router.put('/', async function(req, res, next) {
+  	let user = {};
+  	user.username = req.body.username;
+  	user.email = req.body.email;
+  	user.password = req.body.password;
+  	user.role = req.body.role;
+  	res.send(await usersRepo.updateUser(user));
+});
+
+router.delete('/:id', async function(req, res, next) {
+  	res.send(await usersRepo.deleteUser(req.params.id));
 });
 
 module.exports = router;
